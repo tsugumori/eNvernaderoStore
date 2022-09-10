bf7649bc082185be7e37397411ca208ae68543e1
Lucas Troncoso <ltroncoso@magyp.gob.ar>
2022-09-10 01:30:44 -0300

    cambios

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import logo from './32006.svg';
+import fondo from './Cosmos - 14244.mp4';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      
       <header className="App-header">
+        <p className="Inicio">e-Nvernadero Store </p>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          e-Nvernadero Store.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <video src={fondo} className="Video" loop autoPlay muted></video>
+        <div className="Overlay"></div>
+
       </header>
     </div>
   );
